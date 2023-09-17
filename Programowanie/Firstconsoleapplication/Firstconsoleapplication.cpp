176719b1c04a08bbb1622ba7b45901f4783fd19d
PioteR23 <pioooter.synowi@gmail.com>
2023-09-17 19:23:14 +0200

    dom

--- Programowanie/Firstconsoleapplication/Firstconsoleapplication.cpp
+++ Programowanie/Firstconsoleapplication/Firstconsoleapplication.cpp
@@ -7,14 +7,22 @@ int main()
 {
     
     using namespace std;
+    string login[20];
+    string password[20];
+    string write_password;
+    string write_login;
+    int x = 0;
+    int h = 0;
+    while (x != 20) {
+
+
+
+    }
+
+
     int h;
     cout << "Hello World!\n";
-    cin >> h;
-    cout << h;
-    cout << h;
-    cout << h;
-    cout << h;
-    cout << h;
+   
     
 
 
@@ -25,13 +33,3 @@ int main()
 
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
