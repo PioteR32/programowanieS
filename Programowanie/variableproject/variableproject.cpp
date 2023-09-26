8677b5ef4cda00b963353eb0b88de423ea8767c7
Uczeń - sala 213 <Ucze - sala 213@213-13>
2023-09-26 09:46:34 +0200

    lekcja

--- Programowanie/variableproject/variableproject.cpp
+++ Programowanie/variableproject/variableproject.cpp
@@ -5,13 +5,60 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    double number_from_user;
+
+
+    using namespace std;
+    cout << " Give  a number";
+    cout << "\n";
+    cin >> number_from_user; cout << "\n";
+    cout << number_from_user << "\n";
+
 }
 
 /* Algorytm - skoñczony zbiór instrukcji, sposób postêpowania prowadz¹cy do rozwi¹zania problemu. 
 * Okreœla te¿ kolejnoœæ instrukcji 
 Zapis algorytmu
+ rysunkowy
+ opis s³owny
+ w punktach 
+ schemat blokowy 
+ kod Ÿród³owy danego jêzyka programowania
+ pseudokod
 
+
+ Zmienna jest to obszer w pamiêci operacyjnej,  w której mo¿na przechowywaæ tylko jedn¹ dan¹, jest to identyfikator.
+  Instrukcja Deklaracji zminnej :
+  typ zmiennej nazwa:
+  Typ zmiennej - wielkoœæ obszaru pamiêci, interpretacja ci¹gu bitów
+ typ wbudowany :
+ int- 4 bajtowa liczba ca³kowita  za znakiem przedzia³ -2 147 483 648 do 2 147 483 648.  
+ short - 2 bajtowa liczba ca³kowita ze znakiem  -32 768 do 32 767
+ long-4 bajtowa liczba ca³kowita  za znakiem przedzia³ -2 147 483 648 do 2 147 483 648. 
+ long long - 8 bajtowa liczba ca³kowita  za znakiem przedzia³ -9 trylionów do 9 trylionów.
+ unsigned - zmienna 0 do 2*max
+
+  float - 4 bajtowa licba rzeczywista, dok³adnoœæ 6-7 cyfr po przecinku;
+  double - 8 bajtowa licba rzeczywista, dok³adnoœæ 15-16 cyfr po przecinku;
+  long double- 12 bajtowa licba rzeczywista, dok³adnoœæ 19-20 liczb po przecinku;
+  Nazwa zmiennej - nazwa obszarów pamiêci, identyfikator.
+  warunki niezbêdne:
+  dozwolone znaki 
+  - alfabet aA - zZ;
+  cyfry arabskie 0 - 9;
+  pod³oga
+   
+pierwszym znakiem nie mo¿e byc cyfra
+unikalny w swoim zakresie widocznoœci 
+nie mo¿e to byc s³owo kluczowe danego jêzyka
+warunki programistów:
+nazwa zmiennej nie mo¿e byc przypadkowa
+je¿eli nazwa sk³ada siê z wielu wyrazów to nazywamy:
+nazwey piszemy po angielsku
+
+pan_kowalski
+lub
+panKowalski 
 
 */ 
 
