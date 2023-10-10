9b6e27a4abd644ccdc4cb7608ad282a63ceee5f2
Uczeń - sala 213 <Ucze - sala 213@213-13>
2023-10-10 09:51:24 +0200

    INSTRUKCJA WARUNKOWA IF

---
+++ Programowanie/funkcje_decyzyjne_IF_ELIF/funkcje_decyzyjne_IF_ELIF.cpp
@@ -0,0 +1,42 @@
+﻿
+
+#include <iostream>
+using namespace std;
+/*
+operatory warunkowe:
+- WIĘKSZE  >
+- MNIEJSZE <
+- MNIEJSZE LUB RÓWNE <=
+- WIĘKSZE LUB RÓWNE >= 
+- RÓWNE ==
+- NIE RÓWNE !=
+
+*/
+void task1()
+{
+	double firstNumber;
+	std::cout << "Podaj liczbę\n";
+	cin >> firstNumber;
+	if (firstNumber < 0)
+	{
+		cout << "\n Liczba ujemna";
+	}
+
+	if (firstNumber > 0)
+	{
+		cout << " \n Liczba dodatnia";
+	}
+
+	if (firstNumber == 0)
+	{
+		cout << "\n Liczba równa 0";
+	}
+
+
+}
+int main()
+{
+	setlocale(LC_CTYPE, "polish");
+	task1();
+}
+
