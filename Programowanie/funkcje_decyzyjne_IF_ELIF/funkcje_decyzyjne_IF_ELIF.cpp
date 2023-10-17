b55ca624a16a8bf99b7d67a303b499d0bd87256d
Uczeń - sala 213 <Ucze - sala 213@213-13>
2023-10-17 09:55:29 +0200

    Lekcja

--- Programowanie/funkcje_decyzyjne_IF_ELIF/funkcje_decyzyjne_IF_ELIF.cpp
+++ Programowanie/funkcje_decyzyjne_IF_ELIF/funkcje_decyzyjne_IF_ELIF.cpp
@@ -143,7 +143,110 @@ void task4()
 		float quoitent = firstNumber / secondNumber;
 	}
 }
-int main()
+
+//. Napisz program, który poprosi użytkownika o podanie dwóch liczb całkowitych i sprawdzi, czy są one równe. Wyświetl odpowiedni komunikat./
+void task5()
+{
+	int firstNumber, secondNumber;
+	std::cout << "Podaj liczbę\n";
+	cin >> firstNumber;
+	std::cout << "Podaj liczbę\n";
+	cin >> secondNumber;
+	if (firstNumber == secondNumber) 
+	{
+		std::cout << "Liczba jest równa liczbie drugiej\n";
+	}
+	else
+	{
+		std::cout << "Liczba nie  jest równa liczbie drugiej\n";
+	}
+
+}
+// Napisz program, który poprosi użytkownika o podanie wieku i sprawdzi, czy osoba jest pełnoletnia. Wyświetl odpowiedni komunikat.
+void task6()
+{
+	int firstNumber;
+	std::cout << "Podaj liczbę\n";
+	cin >> firstNumber;
+	
+	if (18 >= firstNumber )
+	{
+		std::cout << "Pełnoletni\n";
+	}
+	else
+	{
+		std::cout << "niepełnoletni\n";
+	}
+}
+//  Napisz program, który poprosi użytkownika o podanie liczby całkowitej i obliczy jej wartość bezwzględną. Wyświetl wynik.
+void task7() {
+	int firstNumber;
+
+	std::cout << "Podaj liczbę\n";
+	cin >> firstNumber;
+	
+	if (0 <firstNumber )
+	{
+		std::cout << "Liczba bezwzględna tej liczby \n" << firstNumber* -1;
+	}
+	else
+	{
+		std::cout << "Liczba bezwzględna tej liczby \n " << firstNumber;
+	}
+}
+//  Napisz program, który poprosi użytkownika o podanie liczby od 1 do 7 i wyświetli odpowiadający mu dzień tygodnia.
+void task8() {
+	int firstNumber;
+
+	std::cout << "Podaj liczbę\n";
+	cin >> firstNumber;
+	if (firstNumber <= 0 || firstNumber >= 8)
+	{
+		if (firstNumber == 1)
+		{
+			std::cout << "Dzień tygodnia to poniedziałek \n";
+		}
+		if (firstNumber == 2)
+		{
+			std::cout << "Dzień tygodnia to wtorek \n";
+		}
+		if (firstNumber == 3)
+		{
+			std::cout << "Dzień tygodnia to środa \n";
+		}
+		if (firstNumber == 4)
+		{
+			std::cout << "Dzień tygodnia to czwartek \n";
+		}
+		if (firstNumber == 5)
+		{
+			std::cout << "Dzień tygodnia to piątek \n";
+		}
+		if (firstNumber == 6)
+		{
+			std::cout << "Dzień tygodnia to sobota \n";
+			if (firstNumber == 7)
+			{
+				std::cout << "Dzień tygodnia to niedziela \n";
+			}
+
+		} }
+	}
+//  Napisz program, który poprosi użytkownika o podanie dwóch liczb całkowitych i wyświetli większą z nich.
+//  Napisz program, który poprosi użytkownika o podanie roku i sprawdzi, czy jest to rok przestępny. Wyświetl odpowiedni komunikat.
+// Napisz program, który poprosi użytkownika o podanie liczby całkowitej i sprawdzi, czy jest ona podzielna zarówno przez 3, jak i przez 5. Wyświetl odpowiedni komunikat.
+//  Napisz program, który poprosi użytkownika o podanie masy ciała (w kilogramach) i wzrostu (w metrach).
+// to do nad tym Na podstawie tych danych oblicz wskaźnik BMI (Body Mass Index) i wyświetl odpowiedni komunikat informujący o stanie zdrowia.
+//  Napisz program, który poprosi użytkownika o podanie długości trzech odcinków i sprawdzi, czy można zbudować z nich trójkąt. Wyświetl odpowiedni komunikat.
+//  Napisz program, który poprosi użytkownika o podanie liczby dodatniej i obliczy jej pierwiastek kwadratowy, jeśli jest to możliwe. Wyświetl wynik lub odpowiedni komunikat, jeśli liczba jest ujemna.
+//  Program sprawdzający czy podana data jest poprawna (np. sprawdzając, czy dzień jest z zakresu od 1 do 31, miesiąc od 1 do 12 itd.)
+
+
+
+
+
+
+int main();
 {
 	setlocale(LC_CTYPE, "polish");
 	//task1();
