45c2111a06e4a2c4e814f8c1cad20d71055cfd7e
Uczeń - sala 213 <Ucze - sala 213@213-13>
2023-10-17 09:33:03 +0200

    lekcja bez zadań

--- Programowanie/funkcje_decyzyjne_IF_ELIF/funkcje_decyzyjne_IF_ELIF.cpp
+++ Programowanie/funkcje_decyzyjne_IF_ELIF/funkcje_decyzyjne_IF_ELIF.cpp
@@ -1,6 +1,7 @@
 ﻿
 
 #include <iostream>
+
 using namespace std;
 /*
 operatory warunkowe:
@@ -11,6 +12,16 @@ operatory warunkowe:
 - RÓWNE ==
 - NIE RÓWNE !=
 
+operatory logiczne
+&& - AND
+|| - OR
+! - NOT		                     Albo
+XOR - nie w c++
+a	b	a&&b	a||b	!a		a XOR b		
+F   F	  F		F		T			F
+F	T	  F		T		T			T
+T	F	  F		T		F			T
+T	T	  T		T		F			F		
 */
 void task1()
 {
@@ -67,14 +78,75 @@ void task2()
 		cout << " \n Liczba nieparzysta";
 		cout << " \n Liczba nieparzysta";
 	}
+}
 
+	// sprawdź czy liczba jest z zakresu <-1,10)
+void task3()
+{
+		
+	int firstNumber ;
+	
+	std::cout << "Podaj liczbę\n";
+	cin >> firstNumber;
+	if (firstNumber >= 1 && firstNumber < 10)
+	{
+		cout << " liczba jest z zakresu <1,10)";
+	}
+	else
+	{
+		if (firstNumber >= 10) 
+		{
+			cout << " liczba   jest większa od zakresu <1,10)";
+		}
+		else
+		{
+			cout << " liczba   jest mniejsza od zakresu <1,10)";
+		}
+	}
+	// wersja druga
+	
+
+	std::cout << "Podaj liczbę\n";
+	cin >> firstNumber;
+	if (firstNumber >= 1 && firstNumber < 10)
+		cout << " liczba jest z zakresu <1,10)";
+	else
+		if (firstNumber >= 10)
+			cout << " liczba   jest większa od zakresu <1,10)";
+		else
+			cout << " liczba   jest mniejsza od zakresu <1,10)";
+		
+	
+}
+// podziel dwie liczby
+#include <string>
+void task4()
+ 
+
+{
+	string a = " debilem";
+	float firstNumber, secondNumber;
+
+	std::cout << "Podaj liczbę\n";
+	cin >> firstNumber;
+	std::cout << "Podaj liczbę\n";
+	cin >> secondNumber;
 
 	
+	if (secondNumber == 0) 
+	{
+		cout << " Jesteś " << a << "nie dziel przez zero ty" << a;
+	}
+	
+	else
+	{
+		float quoitent = firstNumber / secondNumber;
+	}
 }
 int main()
 {
 	setlocale(LC_CTYPE, "polish");
 	//task1();
-	task2();
+	task3();
 }
 
