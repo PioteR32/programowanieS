37ece408a12e764096a73cd505778efdc36162c4
PioteR23 <pioooter.synowi@gmail.com>
2023-10-21 20:39:03 +0200

    zrobiłem BMI

--- Programowanie/funkcje_decyzyjne_IF_ELIF/funkcje_decyzyjne_IF_ELIF.cpp
+++ Programowanie/funkcje_decyzyjne_IF_ELIF/funkcje_decyzyjne_IF_ELIF.cpp
@@ -300,13 +300,120 @@ void task10()
 			cout << "\n rok  " << firstNumber << " nwm czy  jest przestępny ";
 	}
 	else
-		cout << "\n ty kur... jeb.. próbowałeś wywalic program";
+		cout << "\n ty kur.. jeb.., próbowałeś wywalic program";
 }
 // Napisz program, który poprosi użytkownika o podanie liczby całkowitej i sprawdzi, czy jest ona podzielna zarówno przez 3, jak i przez 5. Wyświetl odpowiedni komunikat.
+void task11()
+{
 
-//  Napisz program, który poprosi użytkownika o podanie masy ciała (w kilogramach) i wzrostu (w metrach).
-// to do nad tym Na podstawie tych danych oblicz wskaźnik BMI (Body Mass Index) i wyświetl odpowiedni komunikat informujący o stanie zdrowia.
-//  Napisz program, który poprosi użytkownika o podanie długości trzech odcinków i sprawdzi, czy można zbudować z nich trójkąt. Wyświetl odpowiedni komunikat.
+	int firstNumber, secondNumber , thirdNumber;
+	std::cout << "Podaj liczbę\n";
+	cin >> firstNumber;
+	secondNumber = firstNumber % 3;
+	thirdNumber = firstNumber % 5;
+
+	if (secondNumber  == 0 && thirdNumber == 0)
+	{
+		cout << "\n Liczba : " << firstNumber << " jest podzielna przez 3 i 5";
+	}
+	else
+	{
+		cout << "\n Liczba  " << firstNumber << " nie jest podzielna przez trzy";
+	}
+
+
+
+}
+/*  Napisz program, który poprosi użytkownika o podanie masy ciała (w kilogramach) i wzrostu (w metrach).
+  Na podstawie tych danych oblicz wskaźnik BMI(Body Mass Index) i wyświetl odpowiedni komunikat informujący o stanie zdrowia
+  poniżej 16 - wygłodzenie
+16 - 16.99 - wychudzenie
+17 - 18.49 - niedowagę
+18.5 - 24.99 - wagę prawidłową
+25.0 - 29.9 - nadwagę
+30.0 - 34.99 - I stopień otyłości
+35.0 - 39.99 - II stopień otyłości
+powyżej 40.0 - otyłość skrajną
+
+Wzór:
+
+BMI = masa/wysokość^2
+*/ 
+void task11()
+{
+	double weightNumber, tallNumber , bmiNumber;
+	std::cout << "Podaj liczbę\n";
+	cin >> weightNumber;
+	std::cout << "Podaj liczbę\n";
+	cin >> tallNumber;
+	if (tallNumber <= 0 || weightNumber <= 0)
+	{
+		bmiNumber = weightNumber / (tallNumber * tallNumber);
+	
+		if (bmiNumber < 16)
+		{
+			cout << "\n BMI = " << bmiNumber << "- jesteś wygłodzony";
+		}
+		else
+		{
+			if (bmiNumber >= 16 && bmiNumber < 17)
+			{
+				cout << "\n BMI = " << bmiNumber << "- jesteś wychudzony ";
+			}
+			else
+			{
+				if (bmiNumber >= 17 && bmiNumber < 18.50)
+				{
+					cout << "\n BMI = " << bmiNumber << "- masz niedowagę ";
+				}
+				else
+				{
+					if (bmiNumber >= 18.5 && bmiNumber < 24.50)
+					{
+						cout << "\n BMI = " << bmiNumber << "- masz  wagę prawidłową ";
+					}
+					else
+					{
+						if (bmiNumber >= 25 && bmiNumber < 30)
+						{
+							cout << "\n BMI = " << bmiNumber << "- masz  nadwagę ";
+						}
+						else
+						{
+							if (bmiNumber >= 30 && bmiNumber < 35)
+							{
+								cout << "\n BMI = " << bmiNumber << "- masz  I stopień otyłości ";
+							}
+							else
+							{
+								if (bmiNumber >= 35 && bmiNumber < 40)
+								{
+									cout << "\n BMI = " << bmiNumber << "- masz  II stopień otyłości ";
+								}
+								else
+								{
+									if (bmiNumber >= 40)
+									{
+										cout << "\n BMI = " << bmiNumber << "- masz  otyłość skrajną";
+									}
+									else
+									{
+
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+}
+/* 
+ Napisz program, który poprosi użytkownika o podanie długości trzech odcinków i sprawdzi, 
+ czy można zbudować z nich trójkąt. Wyświetl odpowiedni komunikat.
+ */
+
 //  Napisz program, który poprosi użytkownika o podanie liczby dodatniej i obliczy jej pierwiastek kwadratowy, jeśli jest to możliwe. Wyświetl wynik lub odpowiedni komunikat, jeśli liczba jest ujemna.
 //  Program sprawdzający czy podana data jest poprawna (np. sprawdzając, czy dzień jest z zakresu od 1 do 31, miesiąc od 1 do 12 itd.)
 
