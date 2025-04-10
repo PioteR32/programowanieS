5ebba06a1e78a33d8963c499856c553759f206b0
q <q>
2025-04-10 12:29:14 +0200

    koniec

--- Programowanie/MaturaTest/MaturaTest.cpp
+++ Programowanie/MaturaTest/MaturaTest.cpp
@@ -7,14 +7,131 @@
 #include <xstring>
 #include <string>
 using namespace std;
+ifstream plik = ifstream("przyklad.txt");
+vector<int > numbers = vector<int>();
+vector<int> reversedNumbers = vector<int>();
+vector<int> absoluteNumbers = vector<int>();
+vector<int> positionsOfPrimeNumbers;
+vector<int> primeNumbers;
+
+const int MAX_PRIME_NUMBER = 5000;
+void ReversingNumber()
+{
+	for (int number : numbers)
+	{
+		vector<int> digits = vector<int>();
+		do
+		{
+			digits.push_back(number % 10);
+			number /= 10;
+		} while (number != 0);
+
+		int power = digits.size() - 1;
+		int reversedNumber = 0;
+
+		for (int num : digits)
+		{
+			reversedNumber += num * pow(10, power);
+			power--;
+		}
+
+		reversedNumbers.push_back(reversedNumber);
+		if (reversedNumber % 17 == 0)
+			std::cout << to_string(reversedNumber) + "\n";
+	}
+}
+
+void SetAbsoluteNumbers()
+{
+	for (int i = 0; i < reversedNumbers.size(); i++)
+	{
+		if (numbers[i] - reversedNumbers[i] < 0)
+		{
+			absoluteNumbers.push_back(-(numbers[i] - reversedNumbers[i]));
+		}
+		else
+			absoluteNumbers.push_back((numbers[i] - reversedNumbers[i]));
+	}
+}
+
+void GeneratePrimeNumbers()
+{
+	int number = 1;
+	while (number < 5000)
+	{
+		number += 1;
+		bool isFirst = true;
+		for (int primeNumber : primeNumbers)
+		{
+			if (number % primeNumber == 0)
+			{
+				isFirst = false;
+				break;
+			}
+		}
+		if (isFirst)
+		{
+			primeNumbers.push_back(number);
+		}
+	}
+}
+
+void FindPrimeNumbers()
+{
+	for (int i = 0; i < reversedNumbers.size(); i++)
+	{
+		for (int primeNumber : primeNumbers)
+		{
+			if (primeNumber > reversedNumbers[i] / 2 && primeNumber > numbers[i] / 2)
+			{
+				positionsOfPrimeNumbers.push_back(i);
+				break;
+			}
+			if (primeNumber < reversedNumbers[i] / 2)
+			{
+				if (reversedNumbers[i] % primeNumber == 0)
+				{
+					break;
+				}
+			}
+			if (primeNumber < numbers[i] / 2)
+				if (numbers[i] % primeNumber == 0)
+				{
+					break;
+				}
+		}
+	}
+}
+
+void PrintFindedNumbers()
+{
+	for (int position : positionsOfPrimeNumbers)
+		cout << to_string(numbers[position]) + "\n";
+}
+//numberOfReapetedNumbers[3] a table to which the number of repeated numbers is written
+void SetReapetedNumbers(int numberOfReapetedNumbers[3])
+{
+	/*int numberOfReapetedNumbers[3] = { 0,0,0 };*/
+	int i = 0;
+	for (int number : numbers)
+	{
+		int numberOfNum = 0;
+		for (int num : numbers)
+		{
+			if (num == number)
+				numberOfNum++;
+		}
+		numberOfReapetedNumbers[numberOfNum - 1]++;
+		i++;
+	}
+
+	numberOfReapetedNumbers[1] /= 2;
+	numberOfReapetedNumbers[2] /= 3;
+}
 
 int main()
 {
-	ifstream plik = ifstream("liczby.txt");
-	vector<int > numbers = vector<int>();
-	vector<int> reversedNumbers = vector<int>();
-	vector<int> negativeNumbers = vector<int>();
-	const int MAX_PRIME_NUMBER = 5000;
+
 	if (plik.is_open())
 	{
 		int addingNumber;
@@ -25,125 +142,36 @@ int main()
 
 		// zad 4.1
 		std::cout << "zad 4.1\n";
-
-		for (int number : numbers)
-		{
-			vector<int> digits = vector<int>();
-			do
-			{
-				digits.push_back(number % 10);
-				number /= 10;
-			} while (number != 0);
-
-			int power = digits.size() - 1;
-			int reversedNumber = 0;
-
-			for (int num : digits)
-			{
-				reversedNumber += num * pow(10, power);
-				power--;
-			}
-
-			reversedNumbers.push_back(reversedNumber);
-			if (reversedNumber % 17 == 0)
-				std::cout << to_string(reversedNumber) + "\n";
-		}
+		ReversingNumber();
 		
 		// zad 4.2
 		std::cout << "zad 4.2\n";
 
-		for (int i = 0; i < reversedNumbers.size(); i++)
-		{
-			if (numbers[i] - reversedNumbers[i] < 0)
-			{
-				negativeNumbers.push_back(-(numbers[i] - reversedNumbers[i]));
-			}
-			else
-				negativeNumbers.push_back((numbers[i] - reversedNumbers[i]));
-		}
-
+		SetAbsoluteNumbers();
 		int position = 0;
-		for (int i = 0; i < negativeNumbers.size(); i++)
+		for (int i = 0; i < absoluteNumbers.size(); i++)
 		{
-			if (negativeNumbers[i] > negativeNumbers[position])
+			if (absoluteNumbers[i] > absoluteNumbers[position])
 			{
 				position = i;
 			}
 		}
-		std::cout << to_string(numbers[position]) + " " + to_string(negativeNumbers[position]) + "\n";
+		std::cout << to_string(numbers[position]) + " " + to_string(absoluteNumbers[position]) + "\n";
 
 		//zad 4.3
 		cout << "zad 4.3\n";
 
-		vector<int> positionsOfPrimeNumbers;
-		vector<int> primeNumbers;
-		int number = 1;
-
-		while (number < 5000)
-		{
-			number += 1;
-			bool isFirst = true;
-			for (int primeNumber : primeNumbers)
-			{
-				if (number % primeNumber == 0)
-				{
-					isFirst = false;
-					break;
-				}
-			}
-			if (isFirst)
-			{
-				primeNumbers.push_back(number);
-			}
-		}
-		for (int i = 1; i < reversedNumbers.size(); i++)
-		{
-			for (int primeNumber : primeNumbers)
-			{
-				if (primeNumber > reversedNumbers[i] / 2 && primeNumber > numbers[i] / 2)
-				{
-					positionsOfPrimeNumbers.push_back(i);
-					break;
-				}
-				if (primeNumber < reversedNumbers[i] / 2)
-				{
-					if (reversedNumbers[i] % primeNumber == 0)
-					{
-						break;
-					}
-				}
-				if (primeNumber < numbers[i] / 2)
-					if (numbers[i] % primeNumber == 0)
-					{
-						break;
-					}
-			}
-		}
-		for (int position : positionsOfPrimeNumbers)
-			cout <<  to_string(numbers[position]) + "\n";
-
+		GeneratePrimeNumbers();
+		FindPrimeNumbers();
+		PrintFindedNumbers();
 		//zad 4.4
 		cout << "zad 4.4\n";
 
-		// numberOfReapetedNumbers[0] = Number of numbers repeated once
+		// numberOfReapetedNumbers[0] = Number of diffrent numbers
 		// numberOfReapetedNumbers[1] = Number of numbers repeated twice
 		// numberOfReapetedNumbers[2] = Number of numbers repeated third
 		int numberOfReapetedNumbers[3] = { 0,0,0 };
-		int i = 0;
-		for (int number : numbers)
-		{
-			int numberOfNum = 0;
-			for (int num : numbers)
-			{
-				if (num == number)
-					numberOfNum++;
-			}
-			numberOfReapetedNumbers[numberOfNum - 1]++;
-			i++;
-		}
-
-		numberOfReapetedNumbers[1] /= 2;
-		numberOfReapetedNumbers[2] /= 3;
+		SetReapetedNumbers(numberOfReapetedNumbers);
 
 		cout << to_string(numberOfReapetedNumbers[0] + numberOfReapetedNumbers[1] + numberOfReapetedNumbers[2]) + " " + to_string(numberOfReapetedNumbers[1]) + " " + to_string(numberOfReapetedNumbers[2]) + " ";
 	}
