ddc6271891ba2b1aad32bc9c2c07f4d97e5fb6e4
Sala213 <Sala213@ezse.pl>
2025-09-11 16:43:59 +0200

    commit

--- Programowanie/FirstConsoleApp/Program.cs
+++ Programowanie/FirstConsoleApp/Program.cs
@@ -5,12 +5,21 @@ string name = "JAn";
 string surname = "Kowalski";
 Console.WriteLine($"{name } {surname}");
 Console.WriteLine(@"PATH\PATH");
-void ParTest(int t)
+void ParTest(object t)
 {
-    Console.WriteLine($"{t}");
+    int tmp = int.Parse(t.ToString());
+    tmp++;
+    t = tmp;
+    Console.WriteLine($"{tmp}");
 }
 
+void ParTest1(out int  t)
+{
 
+    t = 15;
+    Console.WriteLine($"{t}");
+}
 int t = 90;
 
 ParTest(t);
+Console.WriteLine(t);
